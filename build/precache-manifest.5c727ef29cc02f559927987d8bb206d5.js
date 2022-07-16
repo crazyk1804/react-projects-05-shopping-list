9afe8cd2e52c0fc2acd996f72613c262ba2b51ad
CK <crazyk1804@gmail.com>
2022-07-16 21:33:16 +0900

    Initial commit

---
+++ build/precache-manifest.5c727ef29cc02f559927987d8bb206d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d0dc2f5c5d7034bbc612d443852936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe96878205a1eef621f",
+    "url": "/static/js/2.49b91ebd.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.49b91ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d836126cabcec31344",
+    "url": "/static/js/main.06fc2e4d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.06fc2e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8024ef07c0f3d7c1efe",
+    "url": "/static/js/runtime-main.ee5dceec.js"
+  }
+]);
